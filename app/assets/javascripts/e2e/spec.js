0840b23b5697996e80267ccbbc78507b686ce8c3
Carl Oman <coman@prosper.com>
2014-11-25 14:33:55 -0800

    added phantomjs driver for karma

--- app/assets/javascripts/e2e/spec.js
+++
@@ -1,8 +0,0 @@
-// spec.js
-describe('angularjs homepage', function() {
-  it('should have a title', function() {
-    browser.get('http://127.0.0.1:4444/wd/hub');
-
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
-});
